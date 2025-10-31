e7b4f5461e96de5f4c482c2ca3c334536e2d06e4
qgqqq <89883538+qgqqq@users.noreply.github.com>
2025-10-31 19:49:38 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,46 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    mySprite2.setPosition(randint(0, 160), randint(0, 120))
+    info.startCountdown(3)
+    info.changeScoreBy(1)
+})
+let mySprite2: Sprite = null
+let mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f f f f d d d d d e e f . . 
+    . f d d d d f 4 4 4 e e f . . . 
+    . f b b b b f 2 2 2 2 f 4 e . . 
+    . f b b b b f 2 2 2 2 f d 4 . . 
+    . . f c c f 4 5 5 4 4 f 4 4 . . 
+    . . . f f f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+scene.setBackgroundColor(7)
+mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . b b b b . . . . . . 
+    . . . . b b 3 3 3 3 b b . . . . 
+    . . . c b 3 3 3 3 1 1 b c . . . 
+    . . c b 3 3 3 3 3 1 1 1 b c . . 
+    . c b 1 1 1 3 3 3 3 1 1 3 c c . 
+    c b d 1 1 1 3 3 3 3 3 3 3 b b c 
+    c b b d 1 3 3 3 3 3 1 1 1 b b c 
+    c b b b 3 3 1 1 3 3 1 1 d d b c 
+    . c b b b d d 1 1 3 b d d d c . 
+    . . c c b b d d b b b b c c . . 
+    . . . . c c c c c c c c . . . . 
+    . . . . . b b d 1 1 b . . . . . 
+    . . . . . b d d 1 1 b . . . . . 
+    `, SpriteKind.Food)
+controller.moveSprite(mySprite)
+mySprite.setStayInScreen(true)
+mySprite2.setPosition(randint(0, 160), randint(0, 120))
